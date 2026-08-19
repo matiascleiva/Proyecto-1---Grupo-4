8c6d05e798d04db40335e9772af9495e646afd1d
matiascleiva <macampos2@miuandes.cl>
2026-08-19 11:58:10 -0400

    test: agregar test basico de suma y resta

---
+++ test.py
@@ -0,0 +1,10 @@
+def test_suma():
+    assert 1 + 1 == 2
+
+def test_resta():
+    assert 5 - 3 == 2
+
+if __name__ == "__main__":
+    test_suma()
+    test_resta()
+    print("Todos los tests pasaron correctamente.")
